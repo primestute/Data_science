58d73bdd81a4f795be29b370a89c8f9c464e2e88
Thomas Olubunmi <59010421+primestute@users.noreply.github.com>
2023-03-22 13:37:52 +0100

    Add files via upload

---
+++ avo_app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+@st.cache
+def dataframe(path):
+    data = pd.read_csv(path)
+    return data
+
+avocado = dataframe('avocado.csv')
+avocado_stats = avocado.groupby('type')['average_price'].mean()
+st.dataframe(avocado_stats)
+
+st.header('Line chart by geographies')
+
+with st.form('line_chart'):
+    chosen = st.selectbox(label='Geography', options=avocado['geography'].unique())
+    modify = st.form_submit_button('Submit')
+    if modify:
+        filtered_avocado = avocado[avocado['geography'] == chosen]
+        fig = px.line(filtered_avocado,
+                           x='date', y='average_price',
+                           color='type',
+                           title=f'Avocado Prices in {chosen}')
+        fig.update_layout(xaxis_title="Date", yaxis_title="Average Price")
+        st.plotly_chart(fig)
